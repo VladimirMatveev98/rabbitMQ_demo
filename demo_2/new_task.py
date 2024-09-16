87562bd5cb08570365d45d556441858904b00cfc
VladimirMatveev98 <89163729396@list.ru>
2024-09-16 11:07:05 +0300

    added_durable_queues

--- demo_2/new_task.py
+++ demo_2/new_task.py
@@ -3,8 +3,11 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+channel.basic_qos(prefetch_count=1)
 message = ' '.join(sys.argv[1:])
-channel.queue_declare(queue='hello2', auto_delete=True)
-channel.basic_publish(exchange='', routing_key='hello2', body=message)
+channel.queue_declare(queue='task_queue', auto_delete=True, durable=True)
+print(f"Sending message: {message}")
+channel.basic_publish(exchange='', routing_key='task_queue', body=message,
+                      properties=pika.BasicProperties(delivery_mode=2))
 
 connection.close()
--- demo_2/worker.py
+++ demo_2/worker.py
@@ -5,7 +5,7 @@ import pika
 def callback(ch, method, properties, body):
     print(f"[INFO] Received {body}")
     body = str(body)
-    #Количество точек в полученной строке обозначает время выполнения задачи
+    #Количество точек в полученной строке определяет сложность выполнения задачи
     time.sleep(int(body.count('.')))
     print("[INFO] Done!")
     #Подтверждение выполнения полученной задачи
@@ -14,8 +14,9 @@ def callback(ch, method, properties, body):
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello2', auto_delete=True)
-channel.basic_consume(on_message_callback=callback, queue='hello2', auto_ack=False)
+channel.basic_qos(prefetch_count=1)
+channel.queue_declare(queue='task_queue', auto_delete=True, durable=True)
+channel.basic_consume(on_message_callback=callback, queue='task_queue', auto_ack=False)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
